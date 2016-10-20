c14945137d7e5fcf5a45a2f9d4840060868bc4ae
William L. Thomson Jr <wlt@o-sinc.com>
2016-10-20 01:21:04 -0400

    use-cleaner.sh: Initial use-cleaner.sh script to clean use flags
    Cleans unused global USE flag with -c option, otherwise just outputs
    unused global USE flags.

---
+++ use-cleaner.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Copyright 2016 Obsidian-Studios, Inc.
+# Author William L. Thomson Jr.
+#        wlt@o-sinc.com
+#
+# Distributed under the terms of The GNU Public License v3.0 (GPLv3)
+
+# Use flag cleaner, pretty straight forward
+
+source /etc/portage/make.conf
+
+for flag in ${USE}; do
+	equery hasuse "${flag/ -/ }" > /dev/null 2>&1
+	if [[ $? -eq 1 ]]; then
+		echo "Unused use flag -> ${flag}"
+		if [[ "${1}" == "-c" ]]; then
+			euse -P "${flag}"
+		fi
+	fi
+done
